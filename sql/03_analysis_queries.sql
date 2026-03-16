b74e44ac09e83a7b3af98e2d96c2b89b2598e232
Keaton Craig <keaton.craig8@gmail.com>
2026-03-16 12:09:08 -0400

    Add team home vs away win rate analysis query

--- sql/03_analysis_queries.sql
+++ sql/03_analysis_queries.sql
@@ -26,7 +26,7 @@ ORDER BY avg_run_diff DESC;
 
 
 -- ============================================
--- Query 2: Home vs Away Win Rate
+-- Query 2: League Home vs Away Win Rate
 -- ============================================
 
 SELECT
@@ -55,3 +55,97 @@ SELECT
 FROM `mlb-data-pipeline-489220.mlb_pipeline.games_clean`
 GROUP BY month
 ORDER BY month;
+
+
+-- ============================================
+-- Query 4: Team Home Win Rate
+-- ============================================
+
+SELECT
+  home_team,
+  COUNT(*) AS home_games,
+  SUM(CASE WHEN run_diff > 0 THEN 1 ELSE 0 END) AS home_wins,
+  ROUND(
+    SAFE_DIVIDE(
+      SUM(CASE WHEN run_diff > 0 THEN 1 ELSE 0 END),
+      COUNT(*)
+    ),
+    3
+  ) AS home_win_rate
+FROM `mlb-data-pipeline-489220.mlb_pipeline.games_clean`
+GROUP BY home_team
+HAVING COUNT(*) > 70
+ORDER BY home_win_rate DESC;
+
+
+-- ============================================
+-- Query 5: Team Away Win Rate
+-- ============================================
+
+SELECT
+  away_team,
+  COUNT(*) AS away_games,
+  SUM(CASE WHEN run_diff < 0 THEN 1 ELSE 0 END) AS away_wins,
+   ROUND(
+    SAFE_DIVIDE(
+      SUM(CASE WHEN run_diff < 0 THEN 1 ELSE 0 END),
+      COUNT(*)
+    ),
+    3
+  ) AS away_win_rate
+FROM `mlb-data-pipeline-489220.mlb_pipeline.games_clean`
+GROUP BY away_team
+HAVING COUNT(*) > 70
+ORDER BY away_win_rate DESC;
+
+
+-- ============================================
+-- Query 6: Team Home VS Away Win Rate
+-- ============================================
+
+WITH home_stats AS (
+
+SELECT
+  home_team AS team,
+  COUNT(*) AS home_games,
+  SUM(CASE WHEN run_diff > 0 THEN 1 ELSE 0 END) AS home_wins,
+  ROUND(
+    SAFE_DIVIDE(
+      SUM(CASE WHEN run_diff > 0 THEN 1 ELSE 0 END),
+      COUNT(*)
+    ),3
+  ) AS home_win_rate
+FROM `mlb-data-pipeline-489220.mlb_pipeline.games_clean`
+GROUP BY home_team
+HAVING COUNT(*) > 70
+
+),
+
+away_stats AS (
+
+SELECT
+  away_team AS team,
+  COUNT(*) AS away_games,
+  SUM(CASE WHEN run_diff < 0 THEN 1 ELSE 0 END) AS away_wins,
+  ROUND(
+    SAFE_DIVIDE(
+      SUM(CASE WHEN run_diff < 0 THEN 1 ELSE 0 END),
+      COUNT(*)
+    ),3
+  ) AS away_win_rate
+FROM `mlb-data-pipeline-489220.mlb_pipeline.games_clean`
+GROUP BY away_team
+HAVING COUNT(*) > 70
+
+)
+
+SELECT
+  h.team,
+  h.home_games,
+  h.home_win_rate,
+  a.away_games,
+  a.away_win_rate
+FROM home_stats h
+JOIN away_stats a
+ON h.team = a.team
+ORDER BY home_win_rate DESC;
